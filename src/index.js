c00e0a764e7e59007c8cc970b627ccf302c36476
Rommel Rico <me@rommelrico.com>
2018-12-12 08:36:19 -0700

    Adding some HTML to our React component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>;
+    return <div>
+        <label className="label" htmlFor="name">Enter Name:</label>
+        <input id="name" type="text"/>
+        <button style="background-color: blue; color: white;">Submit</button>
+    </div>;
 };
 
 // Take the react component and show it on the screen.
