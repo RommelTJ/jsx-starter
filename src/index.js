0abc4d60b435ead6a74683930ea615ebc03c1fa8
Rommel Rico <me@rommelrico.com>
2018-12-12 08:37:38 -0700

    Styling convention

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>
-        <label className="label" htmlFor="name">Enter Name:</label>
-        <input id="name" type="text"/>
-        <button style="background-color: blue; color: white;">Submit</button>
-    </div>;
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter Name:</label>
+            <input id="name" type="text"/>
+            <button style="background-color: blue; color: white;">Submit</button>
+        </div>
+    );
 };
 
 // Take the react component and show it on the screen.
