caed9322547b217af11d6ee4deee987d0439fe8a
Rommel Rico <me@rommelrico.com>
2018-12-12 17:54:13 -0700

    Undoing the call to JS function

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Sample function
-function getButtonText() {
-    return "Please click me!";
-}
-
 // Create a react component
 const App = () => {
+    const buttonText = "Click Me!";
+
     return (
         <div>
             <label className="label" htmlFor="name">Enter Name:</label>
             <input id="name" type="text"/>
-            <button style={{ backgroundColor: 'blue', color: 'white'}}>{getButtonText()}</button>
+            <button style={{ backgroundColor: 'blue', color: 'white'}}>{buttonText}</button>
         </div>
     );
 };
